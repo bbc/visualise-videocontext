b8c38a2e8d24263bd0a42efc66aaf0007e70f689
Ben Robinson <ben.robinson@bbc.co.uk>
2017-04-13 11:05:49 +0100

    Add sample videocontext

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+import visualise from '../../dist/visualise-videocontext.bundle.js'
+import VideoContext from 'videocontext'
+import bunny from 'file-loader!./big_buck_bunny.mp4'
+
+console.log(visualise)
+const vc = new VideoContext(document.getElementById('canvas'))
+const v1 = vc.video(bunny)
+v1.connect(vc.destination)
+v1.start(0)
+vc.play()
